8897303b51e1ae5abd3fc8a172837d9853808e24
Dmitry Kosmachev <kosma4ev.dmitry@gmail.com>
2025-09-26 18:43:06 +0300

    start project

---
+++ backend/api/serializers.py
@@ -0,0 +1,66 @@
+from django.contrib.auth import get_user_model
+from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+from content.models import Category, Theme, ContentFile
+
+
+User = get_user_model()
+
+
+class UserCreateSerializer(BaseUserCreateSerializer):
+
+    class Meta(BaseUserCreateSerializer.Meta):
+        model = User
+        fields = (
+            "email",
+            "id",
+            "first_name",
+            "last_name",
+            "password",
+        )
+
+    def create(self, validated_data):
+        return super().create(validated_data)
+
+
+class UserListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = (
+            "email",
+            "id",
+            "first_name",
+            "last_name",
+        )
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token["email"] = user.email
+        token["full_name"] = user.get_full_name()
+        return token
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class ThemeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Theme
+        fields = '__all__'
+
+
+class ContentFileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ContentFile
+        fields = '__all__'
---
+++ backend/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic --noinput
+
+cp -r /app/collected_static/. /backend_static/
+
+exec gunicorn --bind 0.0.0.0:8000 backend.wsgi
