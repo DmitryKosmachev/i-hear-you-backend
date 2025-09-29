28573b77a9ef165e47dcb4cc29f02f9696d3ac86
Ivan Sopenko <ivansopenko@gmail.com>
2025-09-29 17:55:54 +0300

    refactor/English

--- backend/tg_bot/management/commands/runbot.py
+++ backend/tg_bot/management/commands/runbot.py
@@ -1,21 +1,22 @@
-# your_app/management/commands/runbot.py
-import os
 import asyncio
 import logging
+import os
+
+from aiogram import Bot, Dispatcher
 from django.core.management.base import BaseCommand
 from django.conf import settings
-from aiogram import Bot, Dispatcher
+
 from tg_bot.handlers import router
 
 
 class Command(BaseCommand):
-    help = 'Запускает Telegram-бота'
+    help = 'Start the Telegram bot'
 
     def add_arguments(self, parser):
         parser.add_argument(
             '--token',
             type=str,
-            help='Telegram Bot Token (приоритет над настройками)',
+            help='Telegram bot token (takes priority over variables)',
         )
 
     def handle(self, *args, **options):
@@ -28,30 +29,31 @@ class Command(BaseCommand):
         if not token:
             self.stderr.write(
                 self.style.ERROR(
-                    '❌ Токен бота не найден. Укажите одним из способов:\n'
-                    '   - Через --token в командной строке\n'
-                    '   - Через переменную окружения TG_TOKEN\n'
-                    '   - Через настройку TELEGRAM_BOT_TOKEN в settings.py'
+                    '❌ Bot token not found. '
+                    'Enter token using one of the methods:\n'
+                    '   - Command argument --token <token>\n'
+                    '   - Via enviromental variable TG_TOKEN\n'
+                    '   - Via setting TELEGRAM_BOT_TOKEN in settings.py'
                 )
             )
             return
 
         self.stdout.write(
-            self.style.SUCCESS(f'Запуск бота с токеном: {token[:10]}...')
+            self.style.SUCCESS(f'Starting bot with token: {token[:10]}...')
         )
 
         try:
             asyncio.run(self.main(token))
         except KeyboardInterrupt:
-            self.stdout.write(self.style.WARNING('🛑 Остановлено пользователем'))
+            self.stdout.write(self.style.WARNING('🛑 Aborted by user'))
         except Exception as e:
-            self.stderr.write(self.style.ERROR(f'❌ Ошибка: {e}'))
+            self.stderr.write(self.style.ERROR(f'❌ Error: {e}'))
 
     async def main(self, token):
         bot = Bot(token=token)
         dp = Dispatcher()
         dp.include_router(router)
 
-        self.stdout.write(self.style.SUCCESS('✅ Бот запущен успешно!'))
+        self.stdout.write(self.style.SUCCESS('✅ Bot started!'))
 
         await dp.start_polling(bot)
