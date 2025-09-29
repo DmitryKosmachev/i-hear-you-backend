9397eb3bc16a1d285ee49b3e15a43908d6f04cb8
Ivan Sopenko <ivansopenko@gmail.com>
2025-09-30 01:37:58 +0300

    streamline token access

--- backend/runbot.py
+++ backend/runbot.py
@@ -1,14 +1,14 @@
-import os
 import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher
+from django.conf import settings
 
 from tg_bot.handlers import router
 
 
 async def main():
-    bot = Bot(token=os.getenv('TG_TOKEN'))
+    bot = Bot(settings.TELEGRAM_BOT_TOKEN)
     dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot)
